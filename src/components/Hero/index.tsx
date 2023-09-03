b86c95e88422e053eccd40a28d36777ae1ab2624
Arpan Das <arpan.das@klythe.com>
2023-09-03 11:26:31 +0530

    My portFolio

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+
+import heroBackground from "../../assets/herobg.png";
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection:'row'
+      }}
+    >
+      <Image
+        src={heroBackground}
+        alt={"background"}
+        style={{ height: "100vh", width: "100vw", margin: "auto" }}
+      ></Image>
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "35%",
+          transform: "translate(-50%, -50%)",
+          display: "flex",
+        }}
+      >
+        <div>
+          <div
+            style={{
+              borderRadius: "100%",
+              background: "#915eff",
+              height: "20px",
+              width: "20px",
+              margin: "20px",
+            }}
+          ></div>
+          <div
+            style={{
+              height: "300px",
+              width: "5px",
+              marginLeft: "27.5px",
+              marginTop: "-20px",
+              background: "linear-gradient(to bottom, #915eff, black)",
+            }}
+          ></div>
+        </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div>
+            <h1
+              style={{
+                color: "white",
+                fontSize: "50px",
+                marginTop: "25px",
+              }}
+            >
+              Hi! I'm{" "}
+              <span
+                style={{
+                  fontFamily: "monospace",
+                  fontWeight: "bolder",
+                  color: "#915eff",
+                }}
+              >
+                Arpan
+              </span>{" "}
+            </h1>
+          </div>
+          <p style={{ color: "white" }}>
+            I'm a passionate and highly skilled MERN (MongoDB, Express.js,
+            React, Node.js) stack developer with a strong background in creating
+            web applications and dynamic user experiences. With a deep
+            understanding of both front-end and back-end technologies, I bring
+            your digital ideas to life by crafting robust, scalable, and
+            efficient solutions.
+          </p>
+        </div>
+        <div >
+          <img
+            style={{ height: "-100%", width: "300%" }}
+            src="https://t3.ftcdn.net/jpg/03/18/60/62/360_F_318606217_Hk8jo2MVoI33SQOkYrfOF929J7JgIP0P.jpg"
+            alt="hero image"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,80 @@
+import Link from "next/link";
+import React from "react";
+import navItems from "../../jsonFiles/navItems.json";
+const Navbar = () => {
+  return (
+
+    <nav
+      className="navbar navbar-expand-lg "
+      style={{
+        background:"rgba(5,8,22,255)",
+        position: "fixed",
+        width:'100%',
+        top: "0",
+        zIndex: "1000",
+      }}
+    >
+      <div className="container-fluid">
+        <p className="navbar-brand" style={{ color: "white" }}>
+          <img
+            style={{
+              height: "40px",
+              width: "40px",
+              borderRadius: "200px",
+              marginLeft: "3%",
+              marginRight: "3%",
+            }}
+            src="https://media.licdn.com/dms/image/D4D03AQGWZkuKXxVCqg/profile-displayphoto-shrink_800_800/0/1690717033455?e=1698883200&v=beta&t=J25DgQy6yuTqKnKPnfapOD6lMEdeAaPcdQFVyN8CYBA"
+            alt="logo"
+          />
+          Full-Stack Developer
+        </p>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+         
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="16"
+            fill="currentColor"
+            className="bi bi-list"
+            viewBox="0 0 16 16"
+            style={{ color: "white" }}
+          >
+            <path
+              fillRule="evenodd"
+              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+            />
+          </svg>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <div style={{ justifyContent: "center", display: "flex", width: "100%" }}>
+            <ul className="navbar-nav">
+              {navItems.map((i) => (
+                <li className="nav-item" >
+                  <Link href={i.Link}>
+                    <button
+                      className="btn  mx-3 text-light "
+                      style={{ width: "100px"}}
+                    >
+                      {i.name}
+                    </button>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
